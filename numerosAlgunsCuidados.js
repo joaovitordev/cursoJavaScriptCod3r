719b8ace9ec89a4c156c4bc9b74b436efc68204d
João Vitor <jv17fc@gmail.com>
2020-05-15 19:50:55 -0300

    Aula de Objeto Math

--- numerosAlgunsCuidados.js
+++ numerosAlgunsCuidados.js
@@ -1,2 +1,12 @@
-console.log(7 / 0) //infinity
+//infinity
+console.log(7 / 0)
+
+// o js já converte para número o valor da string, mas tem que tomar cuidado
+console.log("10" / 2)
+
+// Erro Not a Number (NaN)
+console.log("Show" * 2)
+
+// informação não precisa
+console.log(0.1 + 0.7)
 
