e976e4fe879bfb217cbcfff216b83c02fbea3c12
João Vitor <jv17fc@gmail.com>
2020-05-15 20:19:17 -0300

    Aula de Strings

---
+++ strings.js
@@ -0,0 +1,29 @@
+const escola = "Cod3r"
+
+// pegando 4 indice da string usando a função charAt
+console.log(escola.charAt(4))
+
+// retorna null
+console.log(escola.charAt(5))
+
+// tabela ASC
+console.log(escola.charCodeAt(3))
+
+// vendo o indice que o digito está na varialvel
+console.log(escola.indexOf('r'))
+
+// pegando caracteres
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+// concatenando váriaveis com strings
+console.log('Escola '.concat(escola).concat("1"))
+
+// substituir todas palavras
+console.log(escola.replace(/\w/, 'e'))
+
+// substituir especifico
+console.log(escola.replace(3, 'e'))
+
+// transformando string em array
+console.log('Ana,Maria,Pedro'.split(','))
