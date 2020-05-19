3edff5b15883068c8b26cf8aa4121a5bd1afb273
João Vitor <jv17fc@gmail.com>
2020-05-19 14:51:18 -0300

    Aula de Objetos

---
+++ objeto.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'Celular'
+prod1.preco = 4999.99
+prod1['Desconto'] = 100.80
+
+console.log(prod1)
+
+// objeto
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+}
+
+// JSON
+'{ "nome": "Camisa Polo", "preco": 79.90 }'
