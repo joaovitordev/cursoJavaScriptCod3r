56a1fa8c68b9d57a06d87c6e3f834cc0827b203d
João Vitor <jv17fc@gmail.com>
2020-05-19 13:33:22 -0300

    Aula de estruturas booleanas

---
+++ booleanos.js
@@ -0,0 +1,30 @@
+let isAtivo = false
+console.log(isAtivo)
+
+isAtivo = true
+console.log(isAtivo)
+
+isAtivo = 1
+console.log(!!isAtivo)
+
+console.log('os verdadeiros...')
+console.log(!!3)
+console.log(!!-1)
+console.log(!!' ')
+console.log(!!'Teste')
+console.log(!![])
+console.log(!!{})
+console.log(!!Infinity)
+console.log(!!(isAtivo = true))
+
+console.log('os falsos...')
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+console.log(!!(isAtivo = false))
+
+// se nome for vazio ele coloca o desconhecido senao mostra o nome
+let nome = ''
+console.log(nome || 'Desconhecido')
